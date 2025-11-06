2fc1b49636b3dd2a0a0b8c9ae96d90fac9a6241a
Juan Villa <ing.villa.jimenez@gmail.com>
2025-11-06 11:50:52 -0600

    Agregue un api

--- src/index.js
+++ src/index.js
@@ -8,8 +8,24 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import pizzas from '../data/pizzas.json';
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const urlObject = new URL(request.url);
+
+		if (urlObject.pathname === '/admin') {
+			return new Response('Hola admin');
+		}
+
+		if (urlObject.pathname == '/pizzas') {
+			return new Response(JSON.stringify(pizzas), {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		return new Response('Not found', { status: 404 });
 	},
 };
